9a46e475b75fad19f6daf5c41dfaeae367259dac
siamsamix <rsiamsami@gmail.com>
2026-06-17 17:14:58 +0600

    Feature: add health and damage

--- scene/levels/level_1.gd
+++ scene/levels/level_1.gd
@@ -5,14 +5,29 @@ extends Node2D
 func _ready() -> void:
 	$"CharacterBody2D/pause-menu".hide()
 	$Timer.start()
+	$CharacterBody2D/health.text = "Health: ❤️❤️❤️"
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$CharacterBody2D/Label.text = "Time left: %.2f" %($Timer.time_left)
+	$CharacterBody2D/time_left.text = "Time left: %.2f" %($Timer.time_left)
+	if $CharacterBody2D.life == 0:
+		# replace this with a "Try again" scene or something
+		get_tree().change_scene_to_file("res://scene/main.tscn")
 	pass
 
 
 func _on_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body != $CharacterBody2D:
+		return
+	$CharacterBody2D.life -= 1
+	
+	var tmp = $CharacterBody2D/health.text 
+	$CharacterBody2D/health.text = tmp.left(-2)
+	
 	pass # Replace with function body.
